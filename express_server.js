3fd4be932fff228e4e3445a0174200e3355b621c
Jshen123 <joeshen123@gmail.com>
2017-02-16 20:02:59 +0000

    Added user-registration feature

--- express_server.js
+++ express_server.js
@@ -14,26 +14,106 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+const users = [{
+  randomString :{
+           user_id: 1,
+           email: "first@email.com",
+           password: "1"
+         }
+}];
+
+// let id = 1;
+// function nextId() {
+//   id += 1;
+//   return id;
+// }
+
+
 // app.get("/u/:shortURL", (req, res) => {
 //   let longURL = ;
 //   res.redirect(longURL);
 // });
 
+app.get("/register", (req,res) => {
+  res.render(`urls_register`, users);
+});
 
-app.post("/login", (req,res) => {
-  res.cookie("username", req.body.username);
+app.post("/register", (req,res) => {
+  for (user of users) {
+    if (req.body.email === user.email) {
+      res.end("400");
+    }
+  }
+  if (!req.body.email || !req.body.password) {
+    res.end("400");
+  }
+
+  let randomString = generateRandomString();
+  res.cookie("user_id", randomString)
+  users[randomString] = {
+    user_id: randomString,
+    email: req.body.email,
+    password: req.body.password
+  };
+  users.push(users[randomString]);
   res.redirect(`/`);
 });
 
+// app.get("/cookies", (req, res) => {
+//   let locals = {
+//     users: users,
+//     cookies : req.cookies["user_id"]
+//   };
+//   let test = users[locals.cookies].email;
+//   res.send(test)
+// });
+
+app.post("/login_redirect", (req,res) => {
+  res.redirect(`/login`)
+});
+
+app.post("/register_redirect", (req,res) => {
+  res.redirect(`/register`)
+});
+
+app.get("/login", (req,res) => {
+  let locals = {
+    users: users,
+    cookies : req.cookies["user_id"]
+  };
+  res.render(`urls_login`, locals);
+});
+
+app.post("/login", (req,res) => {
+  // res.cookie("user_id", req.body.username);
+  let locals = {
+    users: users,
+    cookies : req.cookies["user_id"]
+  };
+
+  for (user of users){
+    if (!(req.body.email === user.email) || !(req.body.password === user.password)) {
+      res.end("400");
+    } else {
+    // if (!req.body.password === user.password) {
+    //   res.status(403);
+    // } 
+    res.cookie("user_id", user);
+    res.redirect(`/`);
+    }
+  }
+});
+
 app.post("/logout", (req,res) => {
-  res.clearCookie("username");
-  res.redirect(`/urls_index`);
+  res.clearCookie("user_id");
+  res.redirect(`/urls`);
 });
 
 app.get("/urls", (req, res) => {
   let locals = { 
     urls: urlDatabase,
-    username: req.cookies.username  
+    users: users,
+    cookies : req.cookies["user_id"]  
   };
   res.render("urls_index", locals);
 });
@@ -98,3 +178,8 @@ function generateRandomString() {
 	}  
 	return randomString;
 };
+
+// function generateId() {
+//   id += 1;
+//   return id;
+// }
